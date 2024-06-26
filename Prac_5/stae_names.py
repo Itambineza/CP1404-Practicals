1184db5ffe54de2a3600cb48b64d3cd786a33e00
Itambineza <estherine.uwaseitambineza@my.jcu.edu.au>
2024-06-26 13:36:15 +1000

    Reformatted stae_names.py to follow PEP 8 conventions

--- Prac_5/stae_names.py
+++ Prac_5/stae_names.py
@@ -11,10 +11,14 @@ CODE_TO_NAME = {"QLD": "Queensland",
                 "WA": "Western Australia",
                 "ACT": "Australian Capital Territory",
                 "VIC": "Victoria",
-                "TAS": "Tasmania"}
+                "TAS": "Tasmania"
+                }
+
 print(CODE_TO_NAME)
+
 for code, name in CODE_TO_NAME.items():
     print(f"{code:<3} is {name}")
+
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     try:
